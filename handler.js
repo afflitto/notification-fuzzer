67bf84a57fd2cf6eb92af9a5c2090d0380b98c02
LegoMaster616 <andrew.afflitto@gmail.com>
2019-02-19 16:44:27 -0500

    add follower code

--- handler.js
+++ handler.js
@@ -1,14 +1,42 @@
 'use strict';
 
-module.exports.hello = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+module.exports.follow = (event, context, callback) => {
+  var Twitter = require('twitter');
+  var config = require('./config.js');
+  var T = new Twitter(config);
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+
+  T.get('followers/list', (err, data, response) => {
+    if(err) {
+      console.log(err);
+    } else {
+      let followers = data.users;
+      console.log('followers (data.ids):', followers)
+
+
+      followers.forEach(follower => {
+        let user = {
+          user_id: follower.id_str
+        }
+
+        T.post('friendships/destroy', user, (err, data, response) => {
+          if (err && err[0].code != 34) { //if 34, that means we're not following them in the first place
+            console.log(err, user.user_id)
+          } else {
+            console.log('Unfollowed: ', user.user_id);
+
+            setTimeout(() => {
+              T.post('friendships/create', user, (err, data, response) => {
+                if (err) {
+                  console.log(err, user.user_id)
+                } else {
+                  console.log('Followed: ', user.user_id);
+                }
+              })
+            }, 250)
+          }
+        })
+      })
+    }
+  });
 };
